fe8cc90bb3a5869411571072933537c9d1176f99
yemiwebby <yemiwebby@gmail.com>
2023-10-07 19:51:40 +0100

    initial commit

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,68 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+import User from "App/Models/User";
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+
+export default class UsersController {
+  protected tableName = "users";
+
+  public async create({ request, response }: HttpContextContract) {
+    const newUserSchema = schema.create({
+      username: schema.string([
+        rules.unique({
+          column: "username",
+          table: this.tableName,
+        }),
+        rules.required(),
+      ]),
+      email: schema.string([
+        rules.unique({
+          column: "email",
+          table: this.tableName,
+        }),
+        rules.required(),
+      ]),
+      password: schema.string([rules.required()]),
+    });
+
+    try {
+      const payload = await request.validate({
+        schema: newUserSchema,
+      });
+
+      const user = new User();
+      user.username = payload.username;
+      user.email = payload.email;
+      user.password = payload.password;
+
+      let create_user = await user.save();
+
+      let return_body = {
+        success: true,
+        details: create_user,
+        message: "User Successully created",
+      };
+
+      response.send(return_body);
+    } catch (error) {
+      console.log(error.toString());
+      return response.status(500).send({
+        success: false,
+        message: error.messages,
+      });
+    }
+  } //create
+
+  public async fetch({ request, response }: HttpContextContract) {
+    try {
+      const users = await User.query();
+
+      response.send(users);
+    } catch (error) {
+      return response.status(500).send({
+        success: false,
+        message: error.toString(),
+      });
+    }
+  } //fetch
+}
---
+++ config/database.ts
@@ -0,0 +1,70 @@
+/**
+ * Config source: https://git.io/JesV9
+ *
+ * Feel free to let us know via PR, if you find something broken in this config
+ * file.
+ */
+
+import Env from "@ioc:Adonis/Core/Env";
+import Application from "@ioc:Adonis/Core/Application";
+import type { DatabaseConfig } from "@ioc:Adonis/Lucid/Database";
+import URL from "url-parse";
+const PROD_MYSQL_DB = new URL(Env.get("JAWSDB_URL"));
+
+const databaseConfig: DatabaseConfig = {
+  /*
+  |--------------------------------------------------------------------------
+  | Connection
+  |--------------------------------------------------------------------------
+  |
+  | The primary connection for making database queries across the application
+  | You can use any key from the `connections` object defined in this same
+  | file.
+  |
+  */
+  connection: Env.get("DB_CONNECTION"),
+
+  connections: {
+    /*
+    |--------------------------------------------------------------------------
+    | SQLite
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for the SQLite database.  Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i sqlite3
+    |
+    */
+    sqlite: {
+      client: "sqlite",
+      connection: {
+        filename: Application.tmpPath("db.sqlite3"),
+      },
+      pool: {
+        afterCreate: (conn, cb) => {
+          conn.run("PRAGMA foreign_keys=true", cb);
+        },
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      useNullAsDefault: true,
+      healthCheck: false,
+      debug: false,
+    },
+    mysql: {
+      client: "mysql",
+      connection: {
+        host: Env.get("DB_HOST", PROD_MYSQL_DB.host),
+        port: Env.get("DB_PORT", ""),
+        user: Env.get("DB_USER", PROD_MYSQL_DB.username),
+        password: Env.get("DB_PASSWORD", PROD_MYSQL_DB.password),
+        database: Env.get("DB_DATABASE", PROD_MYSQL_DB.pathname.substr(1)),
+      },
+      debug: Env.get("DB_DEBUG", false),
+    },
+  },
+};
+
+export default databaseConfig;
